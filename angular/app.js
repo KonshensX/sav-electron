d22d89918c3c3685fa9cf6a191aaca8229ab9c52
Mohammed BAZA <mohamedbaza16@gmail.com>
2017-03-31 15:58:24 +0100

    Working on the UI

--- angular/app.js
+++ angular/app.js
@@ -1,26 +1,32 @@
 var app = angular.module('myApp', []);
 
-app.controller ('searchController', function ($scope) {
+app.controller ('searchController', function ($scope, $http, $interval) {
 
-    $scope.values = [
-      {
-        name: "mohammed",
-        age: 11
-      },
-      {
-        name: "yasser",
-        age: 32
-      },
-      {
-        name: "Sandoukhan",
-        age: 22
-      }
-    ];
+    $scope.loading = true
+
+    $scope.values = $http({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/posts'
+      }).then((response) => {
+        $interval(function () {
+          $scope.values = response.data
+          $scope.loading = false
+        }, 2000)
+      })
 
     $scope.add = function () {
         $scope.values.push({
           name: $scope.person.name,
           age: $scope.person.age
          });
+    }
+
+    $scope.getData = function () {
+      $http({
+        method: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/posts'
+      }).then((response) => {
+        $scope.values = response.data
+      })
     }
 });
